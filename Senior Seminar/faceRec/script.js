e0d3cf472ccbb3e3093a891b0d7922e40f24c7dc
Kionte Baker <kionte.baker@blackburn.edu>
2022-03-31 13:55:26 -0500

    a few changes

--- Senior Seminar/faceRec/script.js
+++ Senior Seminar/faceRec/script.js
@@ -1,10 +1,19 @@
 //Faceapi official documentation: https://justadudewhohacks.github.io/face-api.js/docs/index.html
 const video = document.getElementById('video')
+const submitButton = document.getElementById('submit')
+const input = document.getElementById('name')
+const startButton = document.getElementById('start')
 const constraints = {
   audio: false,
   video: true
 };
 
+let text = 'null/unknown'
+submitButton.onclick = async () => {
+ text = input.value
+}
+
+startButton.onclick = async() => {
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
@@ -35,9 +44,37 @@ video.addEventListener('play', () => {
  setInterval(async () => {
   const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
   const resOptions = faceapi.resizeResults(detections, displaySize);
-  
 
+  
+  const anchor = {x: video.width, y: video.height-45}
+  const drawOptions = {
+    anchorPosition: 'TOP_LEFT'
+  }
+  
+  const onBox = new faceapi.draw.DrawTextField(text, anchor, drawOptions)
+  
   canvas.getContext("2d").clearRect(0,0,canvas.width,canvas.height);
+  
   faceapi.draw.drawDetections(canvas, resOptions);
+  onBox.draw(canvas)
+
  }, 100)
-})
+})}
+
+
+async function confirmUser(){
+  let answer = confirm('Are you speaking with your intended person?\n Okay for Yes\n Cancel for No')
+
+  if (answer == false){
+    let newName = prompt('What is their name');
+    text = newName;
+  }else{
+    
+  }
+}
+
+function intervalConfirm(){
+  let intervalID = setInterval(confirmUser, 10000)
+}
+
+intervalConfirm()
